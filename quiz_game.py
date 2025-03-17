ec05279d809b6b703d124647be122171b4196714
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2025-03-16 19:29:16 -0500

    added fist question

--- quiz_game.py
+++ quiz_game.py
@@ -5,4 +5,11 @@ playing = input("Would you like to enter my home? ")
 if playing != "yes":
     quit()
 
-print("excellent let us begin shall we")
+players_name = input("What is your name traveler?")
+
+print("excellent " + players_name + " let us begin shall we")
+
+answer = input(players_name+" What does GPU stand for? A: Gilded papaya unit or B: Graphical Processing Unit")
+
+if answer == "B":
+    print("This is corect... but don't get cocky")
