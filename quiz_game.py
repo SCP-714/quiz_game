a78dc889e6c367caa86d5dde7a224e8cc330d4d1
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2025-03-17 09:30:59 -0500

    Game in working state, added case handeling solution

--- quiz_game.py
+++ quiz_game.py
@@ -1,16 +1,16 @@
-print("welcome to the monkey house")
+print("Welcome to the monkey house")
 
 playing = input("Would you like to enter my home? ")
 
 if playing.lower() != "yes":
     quit()
 
-players_name = input("What is your name traveler?")
+players_name = input("What is your name traveler? ")
 
 print("excellent " + players_name + " let us begin shall we")
 
-answer = input(players_name+" What does GPU stand for?")
-if answer == "graphical processing unit":
+answer = input(players_name+" What does GPU stand for? ")
+if answer.lower() == "graphical processing unit":
     print("This is corect... but don't get cocky")
 else:
     print("This is not correct, pleass try again")    
