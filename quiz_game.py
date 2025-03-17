72d7dfdbe64e95b2e7a35d55e2d2acc7416c76d8
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2025-03-16 21:28:41 -0500

    solved for case restraint

--- quiz_game.py
+++ quiz_game.py
@@ -2,14 +2,15 @@ print("welcome to the monkey house")
 
 playing = input("Would you like to enter my home? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 players_name = input("What is your name traveler?")
 
 print("excellent " + players_name + " let us begin shall we")
 
-answer = input(players_name+" What does GPU stand for? A: Gilded papaya unit or B: Graphical Processing Unit")
-
-if answer == "B":
+answer = input(players_name+" What does GPU stand for?")
+if answer == "graphical processing unit":
     print("This is corect... but don't get cocky")
+else:
+    print("This is not correct, pleass try again")    
