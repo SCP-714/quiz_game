717e6640dac8e2acc8eb1d2c9dc86f2f1ffbb065
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2025-03-18 09:29:08 -0500

    working game

--- quiz_game.py
+++ quiz_game.py
@@ -6,11 +6,15 @@ if playing.lower() != "yes":
     quit()
 
 players_name = input("What is your name traveler? ")
+score = 0
 
 print("excellent " + players_name + " let us begin shall we")
 
 answer = input(players_name+" What does GPU stand for? ")
 if answer.lower() == "graphical processing unit":
     print("This is corect... but don't get cocky")
+    score += 1
 else:
     print("This is not correct, pleass try again")    
+
+print("Here is your score!: " + str(score))
